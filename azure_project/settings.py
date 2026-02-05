86be7880958143f166206d1530ee3672a65b65dc
Phat-pc <phatcharida.tasai@gmail.com>
2026-02-05 21:38:19 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'HOST': 'phatcharida-admin.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
+            'driver': 'ODBC Driver 17 for SQL Server',
         },
     },
 }
