a5a507451f1df8a7c766447ed593b2b08ddadb44
PHEERAWITpilot <martrakulpheerawit@gmail.com>
2026-02-12 15:31:33 +0700

    Fix startup.sh: set PYTHONPATH and DJANGO_SETTINGS_MODULE

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export PYTHONPATH=/home/site/wwwroot:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE=azure_project.settings
 cd /home/site/wwwroot
 pip install -r requirements.txt
 gunicorn azure_project.wsgi --workers 4 --threads 2 --worker-class sync --bind 0.0.0.0:8000
